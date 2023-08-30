c9b6242308b03972e63ac04814839324e3085764
MrUnderSan <MrUnderSan@gmail.com>
2023-08-30 19:44:56 +0300

    completed 01 - 03/1 extra classes

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+type PropsType = {
+    name: string
+    callback: () => void
+}
+
+export const Button = (props: PropsType) => {
+    const onClickHandler = () => {
+        props.callback()
+    }
+    return (
+        <button onClick={onClickHandler}>
+            {props.name}
+        </button>
+    );
+};
