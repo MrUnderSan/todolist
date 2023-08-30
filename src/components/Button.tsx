469154bf4fddfad9e39bca2a20bc7c40a6914745
MrUnderSan <MrUnderSan@gmail.com>
2023-08-30 20:58:50 +0300

    add: universal Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type PropsType = {
+    name: string
+    callback: () => void
+}
+
+export const Button :React.FC<PropsType> = ({name, callback}) => {
+    const onClickHandler = () => {
+        callback()
+    }
+    return (
+        <button onClick={onClickHandler}>
+            {name}
+        </button>
+    );
+};
