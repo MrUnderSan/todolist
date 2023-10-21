a990b3b63f08fbce360f3554101870486609228f
MrUnderSan <MrUnderSan@gmail.com>
2023-10-21 16:11:46 +0300

    feat: add 'REMOVE-TASK', 'CHANGE-TASK-STATUS' reducer and test

--- src/state/tasksReducer.test.ts
+++ src/state/tasksReducer.test.ts
@@ -1,9 +1,8 @@
-import {addTaskAC, tasksReducer, TasksType} from './tasksReducer';
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer, TasksType} from './tasksReducer';
 import {v1} from 'uuid';
 
 let todolistId1: string
 let todolistId2: string
-
 
 let tasks: TasksType
 
@@ -13,14 +12,14 @@ beforeEach(() => {
 
     tasks = {
         [todolistId1]: [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true},
-            {id: v1(), title: 'React', isDone: false}
+            {id: '1', title: 'HTML&CSS', isDone: true},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
         ],
         [todolistId2]: [
-            {id: v1(), title: 'Beer', isDone: true},
-            {id: v1(), title: 'Fish', isDone: false},
-            {id: v1(), title: 'Nuts', isDone: false}
+            {id: '1', title: 'Beer', isDone: true},
+            {id: '2', title: 'Fish', isDone: false},
+            {id: '3', title: 'Nuts', isDone: false}
         ]
     }
 })
@@ -35,5 +34,27 @@ test('correct task should be added in correct todolist', () => {
     expect(endState[todolistId1].length).toBe(4)
     expect(endState[todolistId2].length).toBe(3)
     expect(endState[todolistId1][0].title).toBe('Redux')
+
+})
+test('correct task should be removed in correct todolist', () => {
+    const action = removeTaskAC(todolistId1, '1')
+
+    const endState = tasksReducer(tasks, action)
+
+    expect(tasks[todolistId1].length).toBe(3)
+    expect(tasks[todolistId2].length).toBe(3)
+    expect(endState[todolistId1].length).toBe(2)
+    expect(endState[todolistId2].length).toBe(3)
+
+})
+test('task status should be changed in correct task in correct todolist', () => {
+    const action = changeTaskStatusAC(todolistId1, '3', true)
+
+    const endState = tasksReducer(tasks, action)
+
+    expect(tasks[todolistId1][2].isDone).toBeFalsy()
+    expect(tasks[todolistId2][2].isDone).toBeFalsy()
+    expect(endState[todolistId1][2].isDone).toBeTruthy()
+    expect(endState[todolistId2][2].isDone).toBeFalsy()
 
 })
--- src/state/tasksReducer.ts
+++ src/state/tasksReducer.ts
@@ -11,18 +11,41 @@ export type TasksType = {
 }
 
 type AddTaskActionType = ReturnType<typeof addTaskAC>
+type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
 
-type ActionsType = AddTaskActionType
+type ActionsType = AddTaskActionType | RemoveTaskActionType | ChangeTaskStatusActionType
 export const tasksReducer = (state: TasksType, action: ActionsType): TasksType => {
     switch (action.type) {
         case 'ADD-TASK':
             const newTask: TaskType = {id: v1(), title: action.payload.title, isDone: false}
             return {...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]]}
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.payload.todolistId]:
+                    state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)
+            }
+        case 'CHANGE-TASK-STATUS':
+            return {
+                ...state,
+                [action.payload.todolistId]:
+                    state[action.payload.todolistId]
+                        .map(t => t.id === action.payload.taskId ? {...t, isDone: action.payload.isDone} : t)
+            }
         default:
             return state
     }
 }
 
 export const addTaskAC = (todolistId: string, title: string) => (
-    {type: 'ADD-TASK', payload: {todolistId, title}}
+    {type: 'ADD-TASK' as const, payload: {todolistId, title}}
+)
+
+export const removeTaskAC = (todolistId: string, taskId: string) => (
+    {type: 'REMOVE-TASK' as const, payload: {todolistId, taskId}}
+)
+
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => (
+    {type: 'CHANGE-TASK-STATUS' as const, payload: {todolistId, taskId, isDone}}
 )
