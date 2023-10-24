501e4a35d7b4382432105f3501afe3004fa2999f
MrUnderSan <MrUnderSan@gmail.com>
2023-10-24 20:15:39 +0300

    completed 11/03/1.1 - extra classes (Redux)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,85 @@
-import React, {useEffect, useState} from 'react';
-import {NavLink, Outlet} from 'react-router-dom';
+import React, {useReducer, useState} from 'react';
 import './App.css';
-import './App.css';
-import styles from './components/Site.module.css';
-import { useWindowSize } from './helpers/useWindowSize';
-import {S} from './_stylesSC';
+import {TaskType, Todolist} from './Todolist';
+import {AddItemForm} from './AddItemForm';
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
+import {
+    addTodolistAC
+} from './state/todolists-reducer';
+
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from './state/store';
+
+export type FilterValuesType = "all" | "active" | "completed";
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
 
 
 function App() {
-    // const [burger, setBurger] = useState(false)
 
-    const size = useWindowSize();
-    console.log(size)
+    const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
+    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+
+    const dispatch = useDispatch();
+
+    function addTodolist(title: string) {
+        dispatch(addTodolistAC(title))
+    }
 
     return (
-        // <div>
-        //     hello
-        //     <Outlet />
-        //     {/*<Site/>*/}
-        // </div>
-        <>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                {size > 768
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: "20px"}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+                <Grid container spacing={3}>
+                    {
+                        todolists.map(tl => {
+                            let allTodolistTasks = tasks[tl.id];
+                            let tasksForTodolist = allTodolistTasks;
 
-                    ? <div className={styles.nav}>
-                        <S.NavWrapper><NavLink to={'/page/0'}>PageOne</NavLink></S.NavWrapper>
-                        <S.NavWrapper><NavLink to={'/page/1'}>PageTwo</NavLink></S.NavWrapper>
-                        <S.NavWrapper><NavLink to={'/page/2'}>PageThree</NavLink></S.NavWrapper>
-                        <S.NavWrapper><NavLink to={'/protected'}>Protected</NavLink></S.NavWrapper>
-                    </div>
-                    : <div></div>
-                }
+                            if (tl.filter === "active") {
+                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+                            }
+                            if (tl.filter === "completed") {
+                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+                            }
 
-                <div className={styles.content}>
-                    <Outlet/>
-                </div>
-            </div>
-        </>
+                            return <Grid item key={tl.id}>
+                                <Paper style={{padding: "10px"}}>
+                                    <Todolist
+                                        id={tl.id}
+                                        title={tl.title}
+                                        tasks={tasksForTodolist}
+                                        filter={tl.filter}
+                                    />
+                                </Paper>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Container>
+        </div>
     );
 }
-
 
 export default App;
